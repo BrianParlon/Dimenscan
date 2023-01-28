aef17e88470b19f12c942400d2da10ccfe114254
Brian <b.parlon@gmail.com>
2023-01-28 16:46:07 +0000

    When user enters dimensions they can search on a website it gives them back items that would only fit this space. does not take converted figures currently

--- app/src/main/java/com/example/dimenscan/ManualEntry.java
+++ app/src/main/java/com/example/dimenscan/ManualEntry.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.sax.StartElementListener;
 import android.view.View;
@@ -11,6 +12,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -19,10 +27,11 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import org.json.JSONObject;
 import org.w3c.dom.Text;
 
 public class ManualEntry extends AppCompatActivity implements View.OnClickListener {
-    Button conv, submit, reset;
+    Button conv, submit, reset, online;
     EditText length, height, width;
     TextView lengCon, heightCon, widthCon;
     private FirebaseUser mUser;
@@ -43,6 +52,9 @@ public class ManualEntry extends AppCompatActivity implements View.OnClickListen
         mUser = FirebaseAuth.getInstance().getCurrentUser();
         reference  = FirebaseDatabase.getInstance().getReference().child("Dimensions").child(onlineUserId);
         userId = mUser.getUid();
+
+        online = (Button) findViewById(R.id.submitBtn2);
+        online.setOnClickListener(this);
 
         conv = (Button) findViewById(R.id.convBtn);
         conv.setOnClickListener(this);
@@ -77,12 +89,37 @@ public class ManualEntry extends AppCompatActivity implements View.OnClickListen
                 Toast.makeText(ManualEntry.this,"Dimensions reset",Toast.LENGTH_LONG).show();
                 startActivity(new Intent(this,ManualEntry.class));
                 break;
+
+            case R.id.submitBtn2:
+                Toast.makeText(ManualEntry.this,"Searching dimensions",Toast.LENGTH_LONG).show();
+                searching();
+                break;
             case R.id.submitBtn:
                 saveDimensions();
                 Toast.makeText(ManualEntry.this,"Dimensions submitted",Toast.LENGTH_LONG).show();
                 break;
 
         }
+    }
+
+    private void searching() {
+
+//
+
+        TextView textLength = findViewById(R.id.editTextTextPersonName7);
+        TextView textHeight = findViewById(R.id.editTextTextPersonName11);
+        TextView textWidth = findViewById(R.id.editTextTextPersonName12);
+
+        String lengthSearch = textLength.getText().toString().trim();
+        String widthSearch = textWidth.getText().toString().trim();
+
+        goLink("https://www.wayfair.ie/filters/furniture/sb2/desks-c1774332-p86169~0~"+lengthSearch+"-p86170~0~"+widthSearch+".html");
+
+
+    }
+    private void goLink(String s) {
+        Uri uri = Uri.parse(s);
+        startActivity(new Intent(Intent.ACTION_VIEW,uri));
     }
 
     private void saveDimensions(){
